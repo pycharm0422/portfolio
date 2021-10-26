d715dd4fa594dcb446cdf9e740b9b2452b9e8e21
Faraz <pycharm0422@gmail.com>
2021-10-27 05:21:35 +0530

    first commit

---
+++ main/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Skill, UserProfile, ContactProfile, Testimonial, Media, Portfolio, Certificate, Blog
+
+admin.site.register(Skill)
+admin.site.register(UserProfile)
+admin.site.register(ContactProfile)
+admin.site.register(Testimonial)
+admin.site.register(Media)
+admin.site.register(Portfolio)
+admin.site.register(Certificate)
+admin.site.register(Blog)
+
