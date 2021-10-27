7995098438c22cbac132527892c17fb63e577755
Faraz <pycharm0422@gmail.com>
2021-10-27 12:32:45 +0530

    working on frontend

--- main/views.py
+++ main/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from .models import Blog, Portfolio, ContactProfile, Skill, UserProfile, Testimonial, Media, Certificate
+from django.contrib import messages
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "main/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        certificates = Certificate.objects.filter(is_active=True)
+        testimonials = Testimonial.objects.filter(is_active=True)
+        blogs = Blog.objects.filter(is_active=True)
+        portfolio = Portfolio.objects.filter(is_active=True)
+
+        context["testimonials"] = testimonials
+        context["blogs"] = blogs
+        context["portfolio"] = portfolio
+        context["certificates"] = certificates
+
+
+        return context
+
+
+class ContactView(generic.FormView):
+    template_name = "main/contact.html"
+    form_class = ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.save()
+
+        messages.success(self.request, 'Thankyou we will be in touch soon')
+        return super().form_valid(form)
+
+class PortfolioView(generic.ListView):
+    model = Portfolio
+    template_name = "main/portfolio.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
+class PortfolioDetailView(generic.DetailView):
+    model = Portfolio
+    template_name = "main/portfolio-detail.html"
+
+class BlogView(generic.ListView):
+    model = Blog
+    template_name = "main/blog.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+class BlogDetailView(generic.DetailView):
+	model = Blog
+	template_name = "main/blog-detail.html"
